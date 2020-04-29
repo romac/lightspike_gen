0ea08d776f7abd7506a3fb2769be71e9a2528a38
Romain Ruetschi <romain@informal.systems>
2020-04-29 17:06:01 +0200

    add demuxer struct

--- examples/lightclient.rs
+++ examples/lightclient.rs
@@ -1,17 +1,19 @@
 #![allow(unused_variables, unused_imports)]
 
-pub use lightspike_gen::prelude::*;
+use lightspike_gen::demuxer::Demuxer;
+use lightspike_gen::prelude::*;
 
 pub fn main() {
     let store = TrustedStore::new();
     let (trusted_store_reader, trusted_store_writer) = store.split();
 
-    let mut state = State {
+    let state = State {
         trusted_store_reader,
         trusted_store_writer,
     };
 
-    let result = demuxer::verify_height(&mut state, 42);
+    let mut demuxer = Demuxer::new(state);
+
+    let result = demuxer.verify_height(42);
     dbg!(&result);
 }
-
--- src/demuxer.rs
+++ src/demuxer.rs
@@ -8,90 +8,96 @@ pub enum DemuxerError {
     Io(IoError),
 }
 
-// TODO: Introducer Demuxer structs holding state + references to components.
-
-fn run_scheduler(
-    state: &mut State,
-    input: SchedulerInput,
-) -> Result<SchedulerOutput, DemuxerError> {
-    let scheduler = Gen::new(|co| scheduler::process(state.trusted_store_reader(), input, co));
-
-    let result = drain(scheduler, SchedulerResponse::Init, move |req| {
-        handle_request(state, req)
-    })?;
-
-    result.map_err(|e| DemuxerError::Scheduler(e))
+pub struct Demuxer {
+    state: State,
 }
 
-pub fn verify_height(state: &mut State, height: Height) -> Result<Vec<LightBlock>, DemuxerError> {
-    let input = SchedulerInput::VerifyHeight(height);
-    let result = run_scheduler(state, input)?;
+impl Demuxer {
+    pub fn new(state: State) -> Self {
+        Self { state }
+    }
 
-    match result {
-        SchedulerOutput::TrustedStates(trusted_states) => {
-            state.add_trusted_states(trusted_states.clone());
-            Ok(trusted_states)
+    pub fn verify_height(&mut self, height: Height) -> Result<Vec<LightBlock>, DemuxerError> {
+        let input = SchedulerInput::VerifyHeight(height);
+        let result = self.run_scheduler(input)?;
+
+        match result {
+            SchedulerOutput::TrustedStates(trusted_states) => {
+                self.state.add_trusted_states(trusted_states.clone());
+                Ok(trusted_states)
+            }
         }
     }
-}
 
-// FIXME: This should be a method of a Demuxer struct, and state one of its fields
-pub fn verify_light_block(
-    state: &mut State,
-    lb: LightBlock,
-) -> Result<Vec<LightBlock>, DemuxerError> {
-    let input = SchedulerInput::VerifyLightBlock(lb);
-    let result = run_scheduler(state, input)?;
+    pub fn verify_light_block(&mut self, lb: LightBlock) -> Result<Vec<LightBlock>, DemuxerError> {
+        let input = SchedulerInput::VerifyLightBlock(lb);
+        let result = self.run_scheduler(input)?;
 
-    match result {
-        SchedulerOutput::TrustedStates(trusted_states) => {
-            state.add_trusted_states(trusted_states.clone());
-            Ok(trusted_states)
+        match result {
+            SchedulerOutput::TrustedStates(trusted_states) => {
+                self.state.add_trusted_states(trusted_states.clone());
+                Ok(trusted_states)
+            }
         }
     }
-}
 
-// FIXME: This should be a method of a Demuxer struct, and state one of its fields
-fn validate_light_block(state: &mut State, lb: LightBlock) -> Result<LightBlock, DemuxerError> {
-    let input = VerifierInput::VerifyLightBlock(lb);
-    let result = verifier::process(input).map_err(|e| DemuxerError::Verifier(e))?;
-    match result {
-        VerifierOutput::VerifiedLightBlock(lb) => {
-            state.add_valid_light_block(lb.clone());
-            Ok(lb)
+    pub fn validate_light_block(&mut self, lb: LightBlock) -> Result<LightBlock, DemuxerError> {
+        let input = VerifierInput::VerifyLightBlock(lb);
+        let result = verifier::process(input).map_err(|e| DemuxerError::Verifier(e))?;
+
+        match result {
+            VerifierOutput::VerifiedLightBlock(lb) => {
+                self.state.add_valid_light_block(lb.clone());
+                Ok(lb)
+            }
         }
     }
-}
 
-// FIXME: This should be a method of a Demuxer struct
-pub fn fetch_light_block(state: &mut State, height: Height) -> Result<LightBlock, DemuxerError> {
-    let input = IoInput::FetchLightBlock(height);
-    let result = io::process(input).map_err(|e| DemuxerError::Io(e))?;
-    match result {
-        IoOutput::FetchedLightBlock(lb) => {
-            state.add_fetched_light_block(lb.clone());
-            Ok(lb)
+    pub fn fetch_light_block(&mut self, height: Height) -> Result<LightBlock, DemuxerError> {
+        let input = IoInput::FetchLightBlock(height);
+        let result = io::process(input).map_err(|e| DemuxerError::Io(e))?;
+        match result {
+            IoOutput::FetchedLightBlock(lb) => {
+                self.state.add_fetched_light_block(lb.clone());
+                Ok(lb)
+            }
         }
     }
-}
 
-fn handle_request(
-    state: &mut State,
-    request: SchedulerRequest,
-) -> Result<SchedulerResponse, DemuxerError> {
-    match request {
-        SchedulerRequest::GetLightBlock(height) => {
-            fetch_light_block(state, height).map(|lb| SchedulerResponse::LightBlock(lb))
+    fn handle_request(
+        &mut self,
+        request: SchedulerRequest,
+    ) -> Result<SchedulerResponse, DemuxerError> {
+        match request {
+            SchedulerRequest::GetLightBlock(height) => self
+                .fetch_light_block(height)
+                .map(|lb| SchedulerResponse::LightBlock(lb)),
+
+            SchedulerRequest::VerifyLightBlock(lb) => match self.verify_light_block(lb) {
+                Ok(ts) => Ok(SchedulerResponse::Verified(Ok(ts))),
+                Err(DemuxerError::Verifier(err)) => Ok(SchedulerResponse::Verified(Err(err))),
+                Err(err) => Err(err),
+            },
+
+            SchedulerRequest::ValidateLightBlock(lb) => match self.validate_light_block(lb) {
+                Ok(ts) => Ok(SchedulerResponse::Validated(Ok(ts))),
+                Err(DemuxerError::Verifier(err)) => Ok(SchedulerResponse::Validated(Err(err))),
+                Err(err) => Err(err),
+            },
         }
-        SchedulerRequest::VerifyLightBlock(lb) => match verify_light_block(state, lb) {
-            Ok(ts) => Ok(SchedulerResponse::Verified(Ok(ts))),
-            Err(DemuxerError::Verifier(err)) => Ok(SchedulerResponse::Verified(Err(err))),
-            Err(err) => Err(err),
-        },
-        SchedulerRequest::ValidateLightBlock(lb) => match validate_light_block(state, lb) {
-            Ok(ts) => Ok(SchedulerResponse::Validated(Ok(ts))),
-            Err(DemuxerError::Verifier(err)) => Ok(SchedulerResponse::Validated(Err(err))),
-            Err(err) => Err(err),
-        },
+    }
+
+    pub fn run_scheduler(
+        &mut self,
+        input: SchedulerInput,
+    ) -> Result<SchedulerOutput, DemuxerError> {
+        let scheduler =
+            Gen::new(|co| scheduler::process(self.state.trusted_store_reader(), input, co));
+
+        let result = drain(scheduler, SchedulerResponse::Init, move |req| {
+            self.handle_request(req)
+        })?;
+
+        result.map_err(|e| DemuxerError::Scheduler(e))
     }
 }
